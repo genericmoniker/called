beadcaccfbbcf30ee40df9a02409202902cd4cc3
Eric Smith <eric@esmithy.net>
2025-11-22 13:36:56 -0700

    Filter for active missionaries

--- apps/board/views.py
+++ apps/board/views.py
@@ -1,31 +1,28 @@
+from datetime import date, timedelta
+
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q, QuerySet
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 from apps.missionaries.models import Missionary
 
 PAGE_ITEM_COUNT = 6
+DAYS_AFTER_END_DATE = 30  # Keep missionaries on the board for this long after end date.
 
 
 @login_required
 def index(request: HttpRequest) -> HttpResponse:
     """View for the main board page showing missionaries."""
-    count = Missionary.objects.count()
-    if count == 0:
-        context = {
-            "missionaries": [],
-            "next_url": request.build_absolute_uri("?offset=0")
-        }
-        return render(request, "board/empty.html", context)
-
     offset = int(request.GET.get("offset", 0))
+    count, missionaries = _query_missionaries(offset)
     next_offset = offset + PAGE_ITEM_COUNT if offset + PAGE_ITEM_COUNT < count else 0
-    missionaries = Missionary.objects.all()[offset : offset + PAGE_ITEM_COUNT]
     context = {
         "missionaries": missionaries,
-        "next_url": request.build_absolute_uri(f"?offset={next_offset}")
+        "next_url": request.build_absolute_uri(f"?offset={next_offset}"),
     }
-    return render(request, "board/board.html", context)
+    template = "board/board.html" if missionaries else "board/empty.html"
+    return render(request, template, context)
 
 
 def preview(request: HttpRequest) -> HttpResponse:
@@ -36,3 +33,15 @@ def preview(request: HttpRequest) -> HttpResponse:
         "missionary": missionary,
     }
     return render(request, "board/preview.html", context)
+
+
+def _query_missionaries(offset: int) -> tuple[int, QuerySet[Missionary]]:
+    """Load missionaries from the database.
+
+    :param offset: The offset for pagination.
+    :return: A tuple containing the total count of active missionaries and a queryset of
+        missionaries for the current page.
+    """
+    cutoff_date = date.today() - timedelta(days=DAYS_AFTER_END_DATE)  # noqa: DTZ011
+    queryset = Missionary.objects.filter(Q(end_date__gte=cutoff_date))
+    return queryset.count(), queryset[offset : offset + PAGE_ITEM_COUNT]
