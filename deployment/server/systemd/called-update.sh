c519e6d52d824959092455f312d247bbbd492d82
Eric Smith <eric@esmithy.net>
2025-12-28 18:25:36 -0700

    Update the update script

--- deployment/server/systemd/called-update.sh
+++ deployment/server/systemd/called-update.sh
@@ -5,8 +5,16 @@
 
 set -euo pipefail
 
+# Log all output
+exec 1> >(logger --tag called-update --priority user.info)
+exec 2> >(logger --tag called-update --priority user.error)
+
+echo "Starting update check..."
+
 # Ensure the script is run from the correct directory
 cd "$(dirname "$0")/.."
+
+echo "Current directory: $(pwd)"
 
 current_hash=$(git rev-parse HEAD)
 latest_hash=$(git ls-remote origin -h refs/heads/main | cut -f1)
@@ -14,7 +22,7 @@ latest_hash=$(git ls-remote origin -h refs/heads/main | cut -f1)
 if [ "$current_hash" != "$latest_hash" ]; then
     echo "Application update available. Updating to commit hash ${latest_hash}..."
     git pull || { echo "Failed to pull latest changes"; exit 1; }
-    ../../update-app-user.sh || { echo "Failed to run update script"; exit 1; }
+    ./update-app-user.sh || { echo "Failed to run update script"; exit 1; }
     echo "Application updated."
 else
     echo "Application is up to date at commit hash ${current_hash}."
